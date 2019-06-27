77b5583c97a7cf7e560330bc58fb2895ea0a4ca0
Lucky7Tb <Luckytri101@gmail.com>
2019-06-27 21:43:50 +0700

    modified sidenav

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -14,13 +14,13 @@
     <div class="preloader" id="preloader"></div>
     <nav class="blue darken-1">
         <div class="nav-wrapper container">
-            <a href="#!" class="brand-logo"><img src="<?= base_url('assets/img/Logo SMKN 1 Katapang.png') ?>" class="img-responsive" width="50" style="margin-top:5px" alt="profile"></a>
+            <a href="#!" class="brand-logo center"><img src="<?= base_url('assets/img/Logo SMKN 1 Katapang.png') ?>" class="img-responsive" width="50" style="margin-top:5px" alt="profile"></a>
             <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
                 <li><a id="test" href="">Link1</a></li>
                 <li><a href="">Link2</a></li>
                 <li><a href="<?= site_url('auth/register') ?>">Add User</a></li>
-                <li><img src="<?= base_url('assets/img/IMG-20190520-WA0017.jpg') ?>" class="dropdown-trigger circle img-responsive" width="50" height="50" style="margin-top:5px" data-target="dropdown" alt="profile"></li>
+                <li><span class="dropdown-trigger" data-target="dropdown"><?= $this->session->userdata('user') ?></span></li>
             </ul>
         </div>
     </nav>
@@ -43,20 +43,31 @@
                 <?php endif; ?>
             </div>
         </li>
-        <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-        <li><a href="#!">Second Link</a></li>
-        <li>
-            <div class="divider"></div>
-        </li>
-        <li><a class="subheader">Subheader</a></li>
-        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+        <ul class="collapsible">
+            <li class="active">
+                <div class="collapsible-header"><i class="material-icons">dashboard</i>Dashboard</div>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="<?= site_url('admin/index')?>">Management Barang</a></li>
+                        <li><a class="waves-effect" href="<?= site_url('admin/room')?>">Management Ruangan</a></li>
+                        <li><a href="#!">Management Jenis Barang</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+                <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+        </ul>
     </ul>
 
     <ul id="dropdown" class="dropdown-content">
-        <li><a href="<?= base_url('auth/logout') ?>">Logout</a></li>
-        <li><a href="#!">two</a></li>
-        <li><a href="#!">three</a></li>
-        <li class="divider" tabindex="5"></li>
+        <li><a href="<?= base_url('auth/logout') ?>"><i class="material-icons">exit_to_app</i> Logout</a></li>
+        <li><a href="#!"><i class="material-icons">settings</i>Setting</a></li>
     </ul>
 
     <div id="content">
