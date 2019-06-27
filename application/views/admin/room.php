d66fb38cfdd2c5e5da69317e4a210f8b9fc6e4cb
Lucky7Tb <Luckytri101@gmail.com>
2019-06-27 21:43:37 +0700

    add room management

---
+++ application/views/admin/room.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="form_room">
+        <div class="row">
+            <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Last Name</label>
+            </div>
+            <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Textarea</label>
+            </div>
+        </div>
+    </div>
+</div>
