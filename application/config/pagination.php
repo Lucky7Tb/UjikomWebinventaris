79decfbe7a6945a2965f2043c8ce8be76ea08e4d
Lucky7Tb <Luckytri101@gmail.com>
2019-06-27 21:41:16 +0700

    Add Pagination config

---
+++ application/config/pagination.php
@@ -0,0 +1,28 @@
+<?php
+
+$config['base_url'] = 'http://localhost/latujikomci/admin/index/';
+
+$config['full_tag_open'] = '<ul class="pagination">';
+$config['full_tag_close'] = '<ul>';
+
+$config['first_link'] = '<i class="material-icons">first_page</i>';
+$config['first_tag_open'] = '<li class="waves-effect waves-blue">';
+$config['first_tag_close'] = '</li>';
+
+$config['last_link'] = '<i class="material-icons">last_page</i>';
+$config['last_tag_open'] = '<li class="waves-effect waves-blue">';
+$config['last_tag_close'] = '</li>';
+
+$config['next_link'] = '<i class="material-icons">chevron_right</i>';
+$config['next_tag_open'] = '<li class="waves-effect waves-blue">';
+$config['next_tag_close'] = '</li>';
+
+$config['prev_link'] = '<i class="material-icons">chevron_left</i>';
+$config['prev_tag_open'] = '<li class="waves-effect waves-blue">';
+$config['prev_tag_close'] = '</li>';
+
+$config['cur_tag_open'] = '<li class="active"><a href="#!">';
+$config['cur_tag_close'] = '</a></li>';
+
+$config['num_tag_open'] = '<li class="waves-effect waves-blue">';
+$config['num_tag_close'] = '</li>';
